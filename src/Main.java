ab4379e8e5e9288f5f9ea3332415085ca004191b
Svetoslav Grigoryev <slavabro228@gmail.com>
2026-05-16 01:37:01 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+//this class Main
+public class Main {
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+    //this method "main" is entry point to our program
+    static void main() {
+
+        /*
+        this instruction
+        print string
+        with greeting
+         */
+        System.out.println("My name is Slava");
+
+    }
 }
